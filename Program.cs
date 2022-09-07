c427f04bc69ea38fd7c250cb1903d570cf26dbcc
Jeevananthan-23 <Jeeva023@outlook.com>
2022-09-07 11:15:19 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using StackExchange.Redis;
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var muxe = ConnectionMultiplexer.Connect("localhost");
+            var db = muxe.GetDatabase();
+            var JSONResult = db.CreateTransaction();
+            db.Execute("JSON.SET", "dog:1", "$", "{\"name\":\"Honey\",\"breed\":\"Greyhound\"}");
+            db.Execute("JSON.GET", "dog:1", "$.breed");
+            //  JSONResult.ExecuteAsync("EXPIRE", "FOO", 50);
+            JSONResult.Execute();
+            Console.WriteLine("I'm ok");
+
+
+        }
+    }
+}
